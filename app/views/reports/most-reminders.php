f084d1b4f9f1657eee7e97dbd778a6e52aef546c
em-au <146881865+em-au@users.noreply.github.com>
2024-07-11 06:44:15 +0000

    Add chart for users with most reminders

--- app/views/reports/most-reminders.php
+++ app/views/reports/most-reminders.php
@@ -50,4 +50,84 @@
     </table>
     </div>
 </div>
+
+<?php
+    foreach($data['reminders'] as $reminder) {
+        $username[] = $reminder['username'];
+        $num_reminders[] = $reminder['Number of Reminders'];
+    }
+?>
+
+
+<div class="container" style="height: 300px; width: 600px;">
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const labels = <?php echo json_encode($username) ?>;
+    const data = {
+      labels: labels,
+      datasets: [{
+        data: <?php echo json_encode($num_reminders) ?>,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }]
+    };
+
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                title: {
+                  display: true,
+                  text: 'Number of Reminders'
+                },
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1
+                }
+            },
+            x: {
+                title: {
+                    display: true,
+                    text: 'Username'
+                }
+            }
+        }
+      },
+    };
+
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+</script>
+
 <?php require_once 'app/views/templates/footer.php' ?>
