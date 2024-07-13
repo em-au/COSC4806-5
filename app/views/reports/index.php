59486cc9b8735496d83a65e8e8c0f325a031d9a5
em-au <146881865+em-au@users.noreply.github.com>
2024-07-13 00:22:59 +0000

    Change layout of cards on reports/index

--- app/views/reports/index.php
+++ app/views/reports/index.php
@@ -24,10 +24,9 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
         <div class="card h-100">
-          <div class="card-body">
-            <h5 class="card-title">
-                <p><i class="fa-solid fa-bell fa-2xl" ></i></p>
-                All Reminders</h5>
+          <div class="card-body" style="text-align: center">
+            <div class="d-flex justify-content-center" style="margin-bottom: 16px"><i class="fa-solid fa-bell fa-3x"></i></div>
+            <h5 class="card-title">All Reminders</h5>
             <p class="card-text">
                 Click the button below to see all incomplete and complete reminders for all users</p>
             <a href="/reports/all_reminders"><button type="button" class="btn btn-primary">
@@ -37,10 +36,9 @@
       </div>
       <div class="col">
         <div class="card h-100">
-          <div class="card-body">
-            <h5 class="card-title">
-                <p><i class="fa-solid fa-user fa-2xl"></i></p>
-                Users by Reminders</h5>
+          <div class="card-body" style="text-align: center">
+            <div class="d-flex justify-content-center" style="margin-bottom: 16px"><i class="fa-solid fa-user fa-3x"></i></div>
+            <h5 class="card-title">Users by Reminders</h5>
             <p class="card-text">Click the button below to see users sorted by number of reminders.</p>
           <a href="/reports/most_reminders"><button type="button" class="btn btn-primary">
           See users</button></a>
@@ -49,10 +47,9 @@
       </div>
       <div class="col">
         <div class="card h-100">
-          <div class="card-body">
-            <h5 class="card-title">
-                <p><i class="fa-solid fa-right-to-bracket fa-2xl"></i></p>
-                Users by Logins</h5>
+          <div class="card-body" style="text-align: center">
+            <div class="d-flex justify-content-center" style="margin-bottom: 16px"><i class="fa-solid fa-right-to-bracket fa-3x"></i></div>
+            <h5 class="card-title">Users by Logins</h5>
             <p class="card-text">Click the button below to see users by number of logins.</p>
           <a href="/reports/number_of_logins"><button type="button" class="btn btn-primary">
             See logins</button></a>
